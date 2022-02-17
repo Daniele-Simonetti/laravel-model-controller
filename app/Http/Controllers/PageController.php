e1a925a997f04841deb16f4538055a1c18064f26
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-17 17:01:11 +0100

    end

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,7 +11,8 @@ class PageController extends Controller
     public function index() {
 
         $movies = Movie::all();
-        dd($movies);
-        return view('home');
+        // dd($movies);
+        $data = ['movies' => $movies];
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.base')
 
 @section('content')
-home
+<main>
+  {{-- @dd($movies); --}}
+  @foreach ($movies as $movie)
+  <div>
+    <h1>Titolo: {{ $movie->title }}</h1>
+    <h2>Date: {{ $movie->date }}</h2>
+    <h3>Nationality: {{ $movie->nationality }}</h3>
+    <h4>Original-title: {{ $movie->original_title }}</h4>
+    <span>Vote: {{ $movie->vote }}</span>
+
+  </div>
+  @endforeach
+</main>
 @endsection
